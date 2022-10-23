2779ecd57bba5336396e727dd5276777e6be2e2a
Rachma <rachmanurhalizastudent@gmail.com>
2022-10-23 19:24:31 +0700

    Add project files.

---
+++ P5_2_1214056/ParentForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_2_1214056
+{
+    public partial class FileMenuItem : Form
+    {
+        public FileMenuItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MdiMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void windowsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CassadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void OutputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
